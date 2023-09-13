8642ea8c96b56646b3e464bcbe372ed2e55d6a3d
Lourenco Werneck <lourencovw@gmail.com>
2023-09-13 18:18:19 -0300

    Create students

--- client/app/students/create/page.tsx
+++ client/app/students/create/page.tsx
@@ -1,15 +1,67 @@
+"use client";
+import { getCourses } from "@/services/courses";
+import { addStudent } from "@/services/students";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Create() {
+    const [courses, setCourses] = useState([]);
+    const [name, setName] = useState('');
+    const [selectedCourses, setSelectedCourses] = useState({});
+
+    const router = useRouter()
+
+
+    useEffect(() => {
+        getCourses()
+            .then(({ data }) => setCourses(data))
+            .catch(err => console.error(err))
+    }, [])
+
+    const handleSubmit = (event) => {
+        let payload = {
+            name,
+            courseIds: courses.filter(item => selectedCourses[item.id]).map(item => item.id)
+        };
+
+        addStudent(payload)
+            .then(({ data }) => setCourses(data))
+            .catch(err => console.error(err))
+            .finally(() => router.push('/students'))
+
+        event.preventDefault();
+    }
+
+    const handleCheckboxChange = (courseId) => {
+        setSelectedCourses((prevState) => ({
+            ...prevState,
+            [courseId]: !prevState[courseId],
+        }))
+    };
+
     return (
-        <form action="">
-            <label htmlFor="name"></label>
-            <input type="text" name="name" id="name" />
-            <label htmlFor="course">Choose a car:</label>
-            <select name="course" id="course">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="opel">Opel</option>
-                <option value="audi">Audi</option>
-            </select>
-        </form>
+        <div className="flex flex-col items-center">
+            <h1 className="text-lg">Create a student</h1>
+
+            <form className="flex flex-col w-2/4" action="" onSubmit={handleSubmit}>
+                <label htmlFor="name"></label>
+                <input className=" text-black" type="text" name="name" id="name" value={name} onChange={(e) => { setName(e.target.value) }} />
+                <hr />
+                <label htmlFor="course">Choose courses:</label>
+                {courses.map((course) => (
+                    <div key={course.id}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={selectedCourses[course.id] || false}
+                                onChange={() => handleCheckboxChange(course.id)}
+                            />
+                            {course.name}
+                        </label>
+                    </div>
+                ))}
+                <button type="submit" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add</button>
+            </form>
+        </div>
     );
 }
